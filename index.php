f4c354cb65ea56a9eb19c1ab8b93ffa7c5e5d34e
Dustin Hartje <dustinhartje@gmail.com>
2014-04-21 01:17:46 -0700

    working query displayed

--- index.php
+++ index.php
@@ -2,30 +2,64 @@
 <head>
 </head>
 <body>
-<h1> Hello World!</h1>
 
 <?php
-//$dbhost = "piggy.dustycorners.local";
-$dbhost = "10.20.30.38";
+
+// setup db connection parameters
+$dbhost = "piggy.dustycorners.local";
 $dbname = "ledger";
 $dbuser = "ledgerapp";
 $dbpass = "dw#vG*;b?3FV";
-echo "test";
-// Open DB Connection
-//$dbcon = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,3306);
-$dbcon = mysqli_connect("piggy.dustycorners.local","ledgerapp",$dbpass) or die(mysql_error());
 
-// Check DB Connection
-if (mysqli_connect_errno())
-{
+// connect to db
+$dbcon = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+// verify db connection
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//  echo "<br>" . mysqli_error();
-//  echo "<br>" . mysqli_error_list();
-}
-else
-{
+} else {
   echo "<H2>Connected!</H2>";
 }
+
+// verify we can read relevant entries
+echo "<H1>I am aware of the following accounts:</H1>";
+
+// define query
+$q = "select * from accounts";
+
+// run query and store
+$r = mysqli_query($dbcon,$q);
+
+// get fields from query results
+$f = mysqli_fetch_fields($r);
+
+// start building a table with field names in the header
+echo "<table border=1><tr>";
+
+// list fields
+foreach ($f as $val) {
+  echo "<th>" . $val->name . "</th>";
+  //printf("Name: %s\n",$val->name);
+  //printf("Table: %s\n",$val->table);
+  //printf("max. Len: %s\n",$val->max_length);
+}
+
+echo "</tr>";
+
+//list account names from results
+while ($row = mysqli_fetch_array($r)) {
+  //echo $row['name'] . "<br>";
+  echo "<tr>";
+  for($i=0;$i<count($row);$i++) {
+    echo "<td>" . $row[$i] . "</td>";
+  }
+  echo "</tr>";
+}
+
+
+echo "</table>";
+
+mysqli_close($dbcon);
 ?>
 
 </body>
