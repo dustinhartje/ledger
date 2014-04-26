daae37a6e8d1e26f055708ca3496ab06b2157625
Dustin Hartje <dustinhartje@gmail.com>
2014-04-26 03:03:15 -0700

    bedtime

---
+++ admin.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+
+<!-- This page to be used for admin purposes, not really sure what they will be yet but I'm sure there will be some -->
+<h1>Hello World!</h1>
+
+</body>
+</html>
---
+++ manage.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+
+<!-- This page to be used to manage accounts, categories, payees, etc to be specified as parameters during web call -->
+<h1>Hello World!</h1>
+
+</body>
+</html>
