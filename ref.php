0e238f49fe6ff716d3d4f42dcab9323c8b1c08ad
Dustin Hartje <dustinhartje@gmail.com>
2014-04-26 02:53:12 -0700

    seperated reference page

---
+++ ref.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+// setup db connection parameters
+$dbhost = "piggy.dustycorners.local";
+$dbname = "ledger";
+$dbuser = "ledgerapp";
+$dbpass = "dw#vG*;b?3FV";
+
+// connect to db
+$dbcon = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+// verify db connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+function drawqueryresults($connection,$query) {
+  echo '<h1>' . $query . '</h1>';
+  $r = mysqli_query($connection,$query);
+  $fields = mysqli_fetch_fields($r);
+  echo '<table border="1"><tr>';
+  foreach ($fields as $f) {
+    echo '<th>' . $f->name . '</th>';
+  }
+  echo '</tr>';
+
+  while ($row = mysqli_fetch_assoc($r)) {
+    echo '<tr>';
+    foreach ($row as $f => $c) {
+      echo '<td>' . $c . '</td>';
+    }
+    echo '</tr>';
+  }
+  echo '</table>';
+}
+
+drawqueryresults($dbcon,"select * from accounts");
+drawqueryresults($dbcon,"select * from allocations");
+drawqueryresults($dbcon,"select * from categories");
+drawqueryresults($dbcon,"select c1.id, c1.name, c2.name as parent from categories as c1 left join (categories as c2) on c1.childof=c2.id");
+drawqueryresults($dbcon,"select c1.id, c1.name, c2.name as parent, concat(c2.name, ' > ', c1.name) as fullname from categories as c1 left join (categories as c2) on c1.childof=c2.id");
+drawqueryresults($dbcon,"select * from methods");
+drawqueryresults($dbcon,"select * from payees");
+drawqueryresults($dbcon,"select * from shipments");
+drawqueryresults($dbcon,"select * from transactions");
+
+?>
+
+
+
+<?php
+mysqli_close($dbcon);
+?>
+
+</body>
+</html>
